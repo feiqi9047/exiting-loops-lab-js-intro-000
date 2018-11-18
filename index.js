0076da6f65ae2a5f18dc02d149dd708dd6cea252
Fei Qi <feiqi9047@gmail.com>
2018-11-18 23:32:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function breakOut(array, changeValue, stopValue){
   for (let i=0; i<array.length;i++){
     if(array[i] !==stopValue){
       array[i]=changeValue 
-    } break;
+    } break
   }return array
 }
