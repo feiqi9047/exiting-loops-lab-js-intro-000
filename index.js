a6c1869dac561ea32016ea457c3ed66991c6984c
Fei Qi <feiqi9047@gmail.com>
2018-11-18 23:37:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ function keepGoing(array, changeValue, skipValue){
 
 function findBy(array, findFn){
   for (let i=0;i<array.length;i++){
-    if(array[i]=findFn){
-  }
+    if(findFn(array[i]){
+      return array;
+    }retur 'null'
 }
